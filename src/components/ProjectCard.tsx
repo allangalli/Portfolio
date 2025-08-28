fe0e1af666c5e28c5ad13f488a6b0488f9537f45
allangalli <allangalli15@gmail.com>
2025-08-28 01:06:59 -0400

    feat: updated project work images and fixed slide gallery feature in project cards.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -19,6 +19,7 @@ interface ProjectCardProps {
   description: string;
   avatars: { src: string }[];
   link: string;
+  source: string;
 }
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({
@@ -29,6 +30,7 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
   description,
   avatars,
   link,
+  source,
 }) => {
   return (
     <Column fillWidth gap="m">
@@ -79,6 +81,15 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({
                   href={link}
                 >
                   <Text variant="body-default-s">View project</Text>
+                </SmartLink>
+              )}
+              {source && (
+                <SmartLink
+                  suffixIcon="arrowUpRightFromSquare"
+                  style={{ margin: "0", width: "fit-content" }}
+                  href={source}
+                >
+                  <Text variant="body-default-s">View repository</Text>
                 </SmartLink>
               )}
             </Flex>
