60dabd59c6db1774779e5c433cfb99edad553104
allangalli <allangalli15@gmail.com>
2025-08-27 20:33:39 -0400

    feat: added animation to projects preview on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
         </Column>
       </Column>
       <RevealFx translateY="16" delay={0.6}>
-        <Projects range={[1, 1]} />
+        <Projects range={[1, 4]} />
       </RevealFx>
       {routes["/blog"] && (
         <Flex fillWidth gap="24" mobileDirection="column">
@@ -82,7 +82,7 @@ export default function Home() {
           </Flex>
         </Flex>
       )}
-      <Projects range={[4]} />
+      <Projects range={[2]} />
       {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
