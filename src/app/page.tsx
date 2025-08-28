83565a00b9adf2ee0958cf570401bd301a50882c
allangalli <allangalli15@gmail.com>
2025-08-27 20:31:12 -0400

    feat: added animation to projects preview on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
           </Flex>
         </Flex>
       )}
-      <Projects range={[2]} />
+      <Projects range={[4]} />
       {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
